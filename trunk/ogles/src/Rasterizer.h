5bf566fbe95726bac674c0b2858517f9c2b0dc15
hmwill <hmwill@01b124fe-3611-0410-befa-fcf6deab5fe0>
2003-12-16 19:52:01 +0000

    added BSD-style license text

--- trunk/ogles/src/Rasterizer.h
+++ trunk/ogles/src/Rasterizer.h
@@ -17,6 +17,32 @@
 //
 // 10-06-2003		Hans-Martin Will	initial version
 //
+// --------------------------------------------------------------------------
+//
+// Copyright (c) 2004, Hans-Martin Will. All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions are 
+// met:
+// 
+//	 *  Redistributions of source code must retain the above copyright
+// 		notice, this list of conditions and the following disclaimer. 
+//   *	Redistributions in binary form must reproduce the above copyright
+// 		notice, this list of conditions and the following disclaimer in the 
+// 		documentation and/or other materials provided with the distribution. 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, 
+// OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+// THE POSSIBILITY OF SUCH DAMAGE.
+//
 // ==========================================================================
 
 
@@ -132,19 +158,6 @@ namespace EGL {
 			const RasterPos& c);
 
 		// ----------------------------------------------------------------------
-		// Rasterization of triangle scan line
-		// ----------------------------------------------------------------------
-		void RasterScanLine(const EdgePos& start, const EdgePos& end, U32 y);
-
-		// ----------------------------------------------------------------------
-		// Rasterization of fragment
-		// ----------------------------------------------------------------------
-
-		void Fragment(I32 x, I32 y, EGL_Fixed depth, Color color);
-			// will have special cases based on settings
-			// the coordinates are integer coordinates
-
-		// ----------------------------------------------------------------------
 		// State management
 		// ----------------------------------------------------------------------
 
@@ -161,8 +174,21 @@ namespace EGL {
 		//#include "generated_rasterization_function_declarations.h"
 
 	private:
+		// ----------------------------------------------------------------------
+		// Rasterization of triangle scan line
+		// ----------------------------------------------------------------------
+		void RasterScanLine(const EdgePos& start, const EdgePos& end, U32 y);
+
+		// ----------------------------------------------------------------------
+		// Rasterization of fragment
+		// ----------------------------------------------------------------------
+
+		void Fragment(I32 x, I32 y, EGL_Fixed depth, Color color);
+			// will have special cases based on settings
+			// the coordinates are integer coordinates
+
 		Color TexturedColor(const Color& baseColor, EGL_Fixed tu, EGL_Fixed tv);
-		void RasterClippedXTriangle(RasterPos * pos1, RasterPos * pos2, RasterPos * pos3);
+		//void RasterClippedXTriangle(RasterPos * pos1, RasterPos * pos2, RasterPos * pos3);
 		void InitFogTable();
 
 	private:
