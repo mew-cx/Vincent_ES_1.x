690bd6e971cffc660c99b194b38a8dcadd8d893f
Hans-Martin Will <hmwill@users.noreply.github.com>
2004-03-09 04:21:28 +0000

    preserve settings when creating a new texture - is this correct semantics?

--- ogles/src/ContextTexture.cpp
+++ ogles/src/ContextTexture.cpp
@@ -67,6 +67,10 @@ void Context :: BindTexture(GLenum target, GLuint texture) {
 
 			m_Textures.resize(newSize);
 			m_Textures[texture] = new MultiTexture();
+			m_Textures[texture]->SetWrappingModeS(GetRasterizerState()->GetWrappingModeS());
+			m_Textures[texture]->SetWrappingModeT(GetRasterizerState()->GetWrappingModeT());
+			m_Textures[texture]->SetMinFilterMode(GetRasterizerState()->GetMinFilterMode());
+			m_Textures[texture]->SetMagFilterMode(GetRasterizerState()->GetMagFilterMode());
 		}
 	}
 
