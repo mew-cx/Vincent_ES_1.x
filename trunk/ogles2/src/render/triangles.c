cb50177ded99a3faefab21032984761272f8c9bb
hmwill <hmwill@01b124fe-3611-0410-befa-fcf6deab5fe0>
2006-01-26 08:10:38 +0000

    added culling and clipping

--- trunk/ogles2/src/render/triangles.c
+++ trunk/ogles2/src/render/triangles.c
@@ -45,7 +45,229 @@
 void GlesPrepareTriangles(State * state) {
 }
 
+/*
+** Calculate the interpolation vertex between vertices v0 and v1 at relative position coeff
+*/
+static void Interpolate(Vertex * newVertex, const Vertex * v0, const Vertex * v1, GLfloat coeff) {
+	GLuint index;
+
+	/*
+	** TODO: restrict this to the actual number of floats used
+	*/
+	for (index = 0; index < GLES_MAX_VARYING_FLOATS; ++index) {
+		newVertex->data[index] = v1->data[index] + (v0->data[index] - v1->data[index]) * coeff;
+	}
+}
+
+/*
+** Clip a polygon along lower boundary of frustrum
+*/
+static GLuint ClipLow(const Vertex * input[], GLuint inputCount, const Vertex * output[], Vertex ** nextTemporary, GLuint coord) {
+
+	const Vertex * previous;
+	const Vertex * current;
+	GLuint resultCount, index;
+
+	if (inputCount < 3) {
+		return 0;
+	}
+
+	previous = input[inputCount - 1];
+	resultCount = 0;
+
+	for (index = 0; index < inputCount; ++index) {
+
+		current = input[index];
+
+		if (current->data[coord] >= -current->v.position.w) {
+
+			if (previous->data[coord] >= -previous->v.position.w) {
+				// line segment between previous and current is fully contained in cube
+				output[resultCount++] = current;
+				//previous = current;
+			} else {
+				// line segment between previous and current is intersected;
+				// create vertex at intersection, then add current
+				Vertex * newVertex = (*nextTemporary)++;
+				
+				GLfloat c_x = current->data[coord];
+				GLfloat c_w = current->v.position.w;
+				GLfloat p_x = previous->data[coord];
+				GLfloat p_w = previous->v.position.w;
+				GLfloat num = p_w + p_x; 
+				GLfloat denom = (p_w + p_x) - (c_w + c_x);
+
+				Interpolate(newVertex, current, previous, num / denom);
+				newVertex->data[coord] = -newVertex->v.position.w;
+
+				output[resultCount++] = newVertex;
+				output[resultCount++] = current;
+			}
+		} else {
+			if (previous->data[coord] >= -previous->v.position.w) {
+				// line segment between previous and current is intersected;
+				// create vertex at intersection and add it
+				Vertex * newVertex = (*nextTemporary)++;
+				
+				GLfloat c_x = current->data[coord];
+				GLfloat c_w = current->v.position.w;
+				GLfloat p_x = previous->data[coord];
+				GLfloat p_w = previous->v.position.w;
+				GLfloat num = p_w + p_x; 
+				GLfloat denom = (p_w + p_x) - (c_w + c_x);
+				
+				Interpolate(newVertex, current, previous, num / denom);
+				newVertex->data[coord] = -newVertex->v.position.w;
+
+				output[resultCount++] = newVertex;
+			}
+		}
+
+		previous = current;
+	}
+
+	return resultCount;
+}
+
+/*
+** Clip a polygon along lower boundary of frustrum
+*/
+static GLuint ClipHigh(const Vertex * input[], GLuint inputCount, const Vertex * output[], Vertex ** nextTemporary, GLuint coord) {
+
+	const Vertex * previous;
+	const Vertex * current;
+	GLuint resultCount, index;
+
+	if (inputCount < 3) {
+		return 0;
+	}
+
+	previous = input[inputCount - 1];
+	resultCount = 0;
+
+	for (index = 0; index < inputCount; ++index) {
+
+		current = input[index];
+
+		if (current->data[coord] < current->v.position.w) {
+
+			if (previous->data[coord] < previous->v.position.w) {
+				// line segment between previous and current is fully contained in cube
+				output[resultCount++] = current;
+				//previous = current;
+			} else {
+				// line segment between previous and current is intersected;
+				// create vertex at intersection, then add current
+				Vertex * newVertex = (*nextTemporary)++;
+				
+				GLfloat c_x = current->data[coord];
+				GLfloat c_w = current->v.position.w;
+				GLfloat p_x = previous->data[coord];
+				GLfloat p_w = previous->v.position.w;
+				GLfloat num = p_w - p_x; 
+				GLfloat denom = (p_w - p_x) - (c_w - c_x);
+
+				Interpolate(newVertex, current, previous, num / denom);
+				newVertex->data[coord] = newVertex->v.position.w;
+
+				output[resultCount++] = newVertex;
+				output[resultCount++] = current;
+			}
+		} else {
+			if (previous->data[coord] < previous->v.position.w) {
+				// line segment between previous and current is intersected;
+				// create vertex at intersection and add it
+				Vertex * newVertex = (*nextTemporary)++;
+				
+				GLfloat c_x = current->data[coord];
+				GLfloat c_w = current->v.position.w;
+				GLfloat p_x = previous->data[coord];
+				GLfloat p_w = previous->v.position.w;
+				GLfloat num = p_w - p_x; 
+				GLfloat denom = (p_w - p_x) - (c_w - c_x);
+				
+				Interpolate(newVertex, current, previous, num / denom);
+				newVertex->data[coord] = newVertex->v.position.w;
+
+				output[resultCount++] = newVertex;
+			}
+		}
+
+		previous = current;
+	}
+
+	return resultCount;
+}
+
+
 void GlesRenderTriangle(State * state, const Vertex * p1, const Vertex * p2, const Vertex * p3) {
+
+	Vertex temporary[16];					/* space for vertices introduced through clipping	*/
+	const Vertex * array1[16];				/* list of vertices									*/
+	const Vertex * array2[16];				/* list of vertices									*/
+	Vertex * nextTemporary = temporary;		/* pointer for next temporary vertex to use			*/
+	GLuint numVertices = 3;					/* number of vertices								*/
+	GLuint coord;							/* index of coordinate to clip						*/
+
+	/*
+	** Cull triangle based on orientation
+	*/
+	if (state->cullFaceEnabled) {
+		GLfloat det =
+			  p1->v.position.w * p2->v.position.x * p3->v.position.y
+			+ p2->v.position.w * p3->v.position.x * p1->v.position.y
+			+ p3->v.position.w * p1->v.position.x * p2->v.position.y
+			- p1->v.position.w * p3->v.position.x * p2->v.position.y
+			- p2->v.position.w * p1->v.position.x * p3->v.position.y
+			- p3->v.position.w * p2->v.position.x * p1->v.position.y;
+
+		switch (state->cullMode) {
+			case GL_BACK:
+				if ((det < 0.0f) ^ (state->frontFace == GL_CCW))
+					return;
+
+				break;
+
+			case GL_FRONT:
+				if ((det > 0.0f) ^ (state->frontFace == GL_CW))
+					return;
+
+				break;
+
+			default:
+			case GL_FRONT_AND_BACK:
+				return;
+		}
+	}
+
+	/*
+	** Clip triangle to frustrum
+	*/
+
+	array1[0] = p1;
+	array1[1] = p2;
+	array1[2] = p3;
+
+	for (coord = 0; coord < 3; ++coord) {
+		numVertices = ClipLow(array1, numVertices, array2, &nextTemporary, coord);
+		numVertices = ClipHigh(array2, numVertices, array1, &nextTemporary, coord);	
+	}
+
+	/*
+	** Project to screen space and raster
+	*/
+
+	if (numVertices >= 3) {
+#if 0
+		ClipCoordsToWindowCoords(*array1[0]);
+		ClipCoordsToWindowCoords(*array1[1]);
+
+		for (size_t index = 2; index < numVertices; ++index) {
+			ClipCoordsToWindowCoords(*array1[index]);
+			m_Rasterizer->RasterTriangle(*array1[0], *array1[index - 1], *array1[index]);
+		}
+#endif
+	}
 }
 
 void GlesRenderTriangles(State * state, GLint first, GLsizei count, GLenum type, const void * indices) {
