5b7833d3b2f10c50484b79e872ec25af32e97100
houeland <houeland@01b124fe-3611-0410-befa-fcf6deab5fe0>
2006-02-07 14:18:07 +0000

    Updated license text

--- trunk/essl/compiler/ARBCompiler/ARBCompiler.cpp
+++ trunk/essl/compiler/ARBCompiler/ARBCompiler.cpp
@@ -1,3 +1,38 @@
+/*
+//
+//Copyright (C) 2005-2006  Falanx Microsystems AS
+//All rights reserved.
+//
+//Redistribution and use in source and binary forms, with or without
+//modification, are permitted provided that the following conditions
+//are met:
+//
+//    Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//
+//    Redistributions in binary form must reproduce the above
+//    copyright notice, this list of conditions and the following
+//    disclaimer in the documentation and/or other materials provided
+//    with the distribution.
+//
+//    Neither the name of Falanx Microsystems AS nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+//THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+//FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+//COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+//BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+//LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+//ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//POSSIBILITY OF SUCH DAMAGE.
+//
+*/
 //
 //Copyright (C) 2002-2005  3Dlabs Inc. Ltd.
 //All rights reserved.
@@ -69,7 +104,7 @@ int OutputMultipleStrings = 1;
 // Set up the per compile resources
 //
 void GenerateResources(TBuiltInResource& resources)
-{    
+{
 //    resources.maxLights = 32;
 //    resources.maxClipPlanes = 6;
 //    resources.maxTextureUnits = 32;
@@ -87,8 +122,8 @@ void GenerateResources(TBuiltInResource& resources)
 int main(int argc, char* argv[])
 {
 	ShInitialize();
-	
-	cout << "# Vertex program:" << endl;	
+
+	cout << "# Vertex program:" << endl;
 	ShHandle vertcompiler = ShConstructCompiler(EShLangVertex, EDebugOpNone);
 	TBuiltInResource vertresources;
 	GenerateResources(vertresources);
@@ -96,14 +131,14 @@ int main(int argc, char* argv[])
 	ShDestruct(vertcompiler);
 	cout << endl;
 
-	cout << "# Fragment program:" << endl;	
+	cout << "# Fragment program:" << endl;
 	ShHandle fragcompiler = ShConstructCompiler(EShLangFragment, EDebugOpNone);
 	TBuiltInResource fragresources;
 	GenerateResources(fragresources);
 	CompileFile("basic.frag", fragcompiler, EDebugOpNone, &fragresources);
 	ShDestruct(fragcompiler);
 	cout << endl;
-	
+
 	return 0;
 }
 
@@ -139,7 +174,7 @@ bool CompileFile(char *fileName, ShHandle compiler, int debugOptions, const TBui
 //   Malloc a string of sufficient size and read a string into it.
 //
 # define MAX_SOURCE_STRINGS 5
-char** ReadFileData(char *fileName) 
+char** ReadFileData(char *fileName)
 {
     FILE *in    = fopen(fileName, "r");
     char *fdata;
@@ -151,12 +186,12 @@ char** ReadFileData(char *fileName)
         printf("Error: unable to open input file: %s\n", fileName);
         return 0;
     }
-    
+
 	while (fgetc(in) != EOF)
 		count++;
 
 	fseek(in, 0, SEEK_SET);
-	
+
 	if (!(fdata = (char *)malloc(count+2))) {
             printf("Error allocating memory\n");
             return 0;
@@ -171,7 +206,7 @@ char** ReadFileData(char *fileName)
         return_data[0]=(char*)malloc(count+2);
         return_data[0][0]='\0';
         OutputMultipleStrings=0;
-        return return_data;       
+        return return_data;
     }
 
 	int len = (int)(ceil)((float)count/(float)OutputMultipleStrings);
@@ -188,7 +223,7 @@ char** ReadFileData(char *fileName)
                break;
             }
            len = count;
-		}  
+		}
 		++i;
 	}
     return return_data;
