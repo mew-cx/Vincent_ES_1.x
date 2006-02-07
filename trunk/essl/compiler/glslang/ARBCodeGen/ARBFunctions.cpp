7af51662db63de18bb397e789228e91c201f6717
houeland <houeland@01b124fe-3611-0410-befa-fcf6deab5fe0>
2006-02-07 14:57:56 +0000

    Re-added files.

---
+++ trunk/essl/compiler/glslang/ARBCodeGen/ARBFunctions.cpp
@@ -0,0 +1,214 @@
+/*
+//
+//Copyright (C) 2005-2006  Falanx Microsystems AS
+//All rights reserved.
+//
+//Redistribution and use in source and binary forms, with or without
+//modification, are permitted provided that the following conditions
+//are met:
+//
+//    Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//
+//    Redistributions in binary form must reproduce the above
+//    copyright notice, this list of conditions and the following
+//    disclaimer in the documentation and/or other materials provided
+//    with the distribution.
+//
+//    Neither the name of Falanx Microsystems AS nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+//THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+//FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+//COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+//BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+//LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+//ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//POSSIBILITY OF SUCH DAMAGE.
+//
+*/
+#include "ARBFunctions.h"
+
+#include "Util.h"
+#include <vector>
+#include "ARBInstruction.h"
+using namespace std;
+
+//
+// Operators
+//
+
+// Generates vector + vector assembly code
+ARBInstructionList ARBAddition(ARBVar result, ARBVar vec1, ARBVar vec2) {
+	ARBInstructionList list;
+	//                    Instruction name  result  var1  var2
+	list.push_back(ARBInstruction(EInstADD, result, vec1, vec2));
+	return list;
+}
+
+// Generates vector - vector assembly code
+ARBInstructionList ARBSubtraction(ARBVar result, ARBVar vec1, ARBVar vec2) {
+	ARBInstructionList list;
+	list.push_back(ARBInstruction(EInstSUB, result, vec1, vec2));
+	return list;
+}
+
+// Generates vector * vector assembly code
+ARBInstructionList ARBMultiplication(ARBVar result, ARBVar vec1, ARBVar vec2) {
+	ARBInstructionList list;
+	list.push_back(ARBInstruction(EInstMUL, result, vec1, vec2));
+	return list;
+}
+
+// Generates vector = vector assembly code
+ARBInstructionList ARBAssignment(ARBVar result, ARBVar vector) {
+	ARBInstructionList list;
+	list.push_back(ARBInstruction(EInstMOV, result, vector));
+	return list;
+}
+
+//
+// Math Functions
+//
+
+// Generates minimum min(a, b) assembly code
+ARBInstructionList ARBMinimum(ARBVar result, ARBVar vec1, ARBVar vec2) {
+	ARBInstructionList list;
+	list.push_back(ARBInstruction(EInstMIN, result, vec1, vec2));
+	return list;
+}
+
+// Generates maximun max(a, b) assembly code
+ARBInstructionList ARBMaximum(ARBVar result, ARBVar vec1, ARBVar vec2) {
+	ARBInstructionList list;
+	list.push_back(ARBInstruction(EInstMAX, result, vec1, vec2));
+	return list;
+}
+
+// Generates clamp(a, min, max) assembly code
+ARBInstructionList ARBClamp(ARBVar result, ARBVar vec1, ARBVar minVec, ARBVar maxVec) {
+	ARBInstructionList list;
+	list.push_back(ARBInstruction(EInstMAX, result, vec1, minVec));
+	list.push_back(ARBInstruction(EInstMIN, result, result, maxVec));
+	return list;
+}
+
+// Generates absolute abs(a) assembly code
+ARBInstructionList ARBAbsolute(ARBVar result, ARBVar vector) {
+	ARBInstructionList list;
+	list.push_back(ARBInstruction(EInstABS, result, vector));
+	return list;
+}
+
+// Generates square root sqrt(a) assemply code
+ARBInstructionList ARBSquareRoot(ARBVar result, ARBVar vector) {
+	ARBInstructionList list;
+	list.push_back(ARBInstruction(EInstRSQ, result, vector));
+	list.push_back(ARBInstruction(EInstRCP, result, result));
+	return list;
+}
+
+// Generates a raised to the y power pow(a, b) assemply code
+ARBInstructionList ARBPower(ARBVar result, ARBVar vec1, ARBVar vec2) {
+	ARBInstructionList list;
+	list.push_back(ARBInstruction(EInstPOW, result, vec1, vec2));
+	return list;
+}
+
+//
+// Geometric Functions
+//
+
+// Generates the vector length length(a) assembly code
+ARBInstructionList ARBLength(ARBVar result, ARBVar vector, bool component3) {
+	ARBInstructionList list;
+	if(component3) {
+	list.push_back(ARBInstruction(EInstDP3, result, vector, vector));
+	} else {
+	list.push_back(ARBInstruction(EInstDP4, result, vector, vector));
+	}
+	list.push_back(ARBInstruction(EInstRSQ, result, result.swizzle("w")));
+	list.push_back(ARBInstruction(EInstRCP, result, result));
+	return list;
+}
+
+// Generates the distance between two vectors, distance(a, b) assembly code
+ARBInstructionList ARBDistance(ARBVar result, ARBVar vec1, ARBVar vec2, bool component3) {
+	ARBInstructionList list;
+	list.push_back(ARBInstruction(EInstSUB, result, vec1, vec2));
+	if(component3) {
+	list.push_back(ARBInstruction(EInstDP3, result, result, result));
+	} else {
+	list.push_back(ARBInstruction(EInstDP4, result, result, result));
+	}
+	list.push_back(ARBInstruction(EInstRSQ, result, result.swizzle("w")));
+	list.push_back(ARBInstruction(EInstRCP, result, result));
+	return list;
+}
+
+// Generates dot product of 3- and 4-component vectors dot(a, b) assembly code
+ARBInstructionList ARBDot(ARBVar result, ARBVar vec1, ARBVar vec2, bool component3) {
+	ARBInstructionList list;
+	if (component3) {
+		list.push_back(ARBInstruction(EInstDP3, result, vec1, vec2));
+	} else {
+		list.push_back(ARBInstruction(EInstDP4, result, vec1, vec2));
+	}
+	return list;
+}
+
+// Generates 3-component normalized vector
+ARBInstructionList ARB3ComponentNormalize(ARBVar result, ARBVar vector) {
+	ARBInstructionList list;
+	list.push_back(ARBInstruction(EInstDP3, result.swizzle("w"), vector, vector));
+	list.push_back(ARBInstruction(EInstRSQ, result.swizzle("w"), result.swizzle("w")));
+	list.push_back(ARBInstruction(EInstMUL, result.swizzle("xyz"), result.swizzle("w"), vector));
+	return list;
+}
+
+// Generates 4-component normalized vector
+ARBInstructionList ARB4ComponentNormalize(ARBVar result, ARBVar vector) {
+	ARBInstructionList list;
+	list.push_back(ARBInstruction(EInstDP4, result.swizzle("w"), vector, vector));
+	list.push_back(ARBInstruction(EInstRSQ, result.swizzle("w"), result.swizzle("w")));
+	list.push_back(ARBInstruction(EInstMUL, result, result.swizzle("w"), vector));
+	return list;
+}
+
+// Generates matrix * vector assembly code
+ARBInstructionList ARBMatrixTimesVector(ARBVar result, ARBVar vec, ARBVar row0, ARBVar row1, ARBVar row2, ARBVar row3) {
+	ARBInstructionList list;
+	list.push_back(ARBInstruction(EInstDP4, result.swizzle("x"), vec, row0));
+	list.push_back(ARBInstruction(EInstDP4, result.swizzle("y"), vec, row1));
+	list.push_back(ARBInstruction(EInstDP4, result.swizzle("z"), vec, row2));
+	list.push_back(ARBInstruction(EInstDP4, result.swizzle("w"), vec, row3));
+	return list;
+}
+
+//
+// Built-in functions
+//
+
+// This refers to the build-in functions texture
+ARBInstructionList ARBTexture2D(ARBVar result, vector<ARBVar>& parameters) {
+	// The first parameter must be a sampler2D.
+
+	if (parameters.size() == 2) {					// texture2D(Sampler2D sampler, vec2 coords );
+		ARBInstructionList list;
+		list.push_back(ARBInstruction(EInstTEX, result, parameters[1], parameters[0], ARBVar::NewTextVar("2D")));
+		return list;
+	} else if (parameters.size() == 3) {			// texture2D(Sampler2D sampler, vec2 coords, float bias );
+		ARBInstructionList list = ARBAssignment( parameters[1].swizzle("w"), parameters[2].swizzle("x") );
+		list.push_back(ARBInstruction(EInstTXB, result, parameters[1], ARBVar::NewTextVar("texture[0]"), ARBVar::NewTextVar("2D")));
+		return list;
+	} else {
+		assert(false && "Illegal argument list to ARBTexture2D");
+		return ARBInstructionList();
+	}
+}
---
+++ trunk/essl/compiler/glslang/ARBCodeGen/ARBNodeHandling.cpp
@@ -0,0 +1,403 @@
+/*
+//
+//Copyright (C) 2005-2006  Falanx Microsystems AS
+//All rights reserved.
+//
+//Redistribution and use in source and binary forms, with or without
+//modification, are permitted provided that the following conditions
+//are met:
+//
+//    Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//
+//    Redistributions in binary form must reproduce the above
+//    copyright notice, this list of conditions and the following
+//    disclaimer in the documentation and/or other materials provided
+//    with the distribution.
+//
+//    Neither the name of Falanx Microsystems AS nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+//THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+//FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+//COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+//BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+//LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+//ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//POSSIBILITY OF SUCH DAMAGE.
+//
+*/
+#include "ARBFunctions.h"
+#include "Util.h"
+
+#include "ARBNodeHandling.h"
+
+using namespace std;
+
+// Handles unary math operations like abs, sqrt
+nodeResult ARBUnaryMathNode(TIntermUnary* node) {
+	nodeResult operand = parseNode(node->getOperand());
+
+	if(operand.fullyParsed) {
+		ARBVar result = ARBVar::NewTempVar();
+		ARBInstructionList list;
+		list.append(operand.list);
+
+		switch(node->getOp()) {
+			case EOpAbs: {
+				list.append(ARBAbsolute(result, operand.var));
+				break;
+			}
+			case EOpSqrt: {
+				list.append(ARBSquareRoot(result, operand.var));
+				break;
+			}
+			case EOpLength: {
+				if(node->getSize() == 3) {
+					list.append(ARBLength(result, operand.var, true));
+				} else {
+					list.append(ARBLength(result, operand.var, false));
+				}
+				break;
+			}
+			case EOpNormalize: {
+				if(node->getSize() == 3) {
+					list.append(ARB3ComponentNormalize(result, operand.var));
+				} else {
+					list.append(ARB4ComponentNormalize(result, operand.var));
+				}
+				break;
+			}
+			default: {
+		        failmsg() << "Error in ARBUnaryMathNode, uknown operator " << getopname(node->getOp()) << "\n";
+				return nodeResult(false);
+			}
+		}
+		msg() << " " << getopname(node->getOp()) << operand.var.tostr() << "\n";
+		return nodeResult(result, list);
+	}
+	else {
+		failmsg() << " unhandled function call to " << getopname(node->getOp()) << " on line " << node->getLine() << "\n";
+		failmsg() << "   operand: " << operand.var.tostr() << " { " << (operand.fullyParsed?"parsed":"not parsed") << " }\n";
+		failmsg() << "\n";
+		return nodeResult(false);
+	}
+}
+
+// Handles binary math operators +-* e.g. a + b
+nodeResult ARBMathNode(TIntermBinary* node) {
+	// Generate result variable and instruction list from children
+	nodeResult left = parseNode(node->getLeft());
+	nodeResult right = parseNode(node->getRight());
+
+	if (left.fullyParsed && right.fullyParsed) {
+		ARBVar result = ARBVar::NewTempVar();
+
+		ARBInstructionList list;
+		// Include instructions for generating children
+		list.append(left.list);
+		list.append(right.list);
+		// Add instruction for addition
+
+		switch(node->getOp()) {
+			case EOpAdd:
+				list.append(ARBAddition(result, left.var, right.var));
+				break;
+			case EOpSub:
+				list.append(ARBSubtraction(result, left.var, right.var));
+				break;
+			case EOpMul:
+				list.append(ARBMultiplication(result, left.var, right.var));
+				break;
+			case EOpVectorTimesScalar:
+				list.append(ARBMultiplication(result, left.var, right.var));
+				break;
+			default:
+				failmsg() << "Error in ARBMathNode, uknown operator " << getopname(node->getOp()) << "\n";
+				return nodeResult(false);
+				break;
+		}
+
+		msg() << " " << getopname(node->getOp()) << ", " << result.tostr() << " = " << left.var.tostr() << " op " << right.var.tostr() << "\n";
+		return nodeResult(result, list);
+	} else {
+		failmsg() << " unhandled " << getopname(node->getOp()) << " , [" << ARBVar::GetNodeVar(node->getLeft()).tostr() << "] + [" << ARBVar::GetNodeVar(node->getRight()).tostr() << "] on line " << node->getLine() << "\n";
+		failmsg() << "   left: " << left.var.tostr() << " { " << (left.fullyParsed?"parsed":"not parsed") << " }\n";
+		failmsg() << "   right: " << right.var.tostr() << " { " << (right.fullyParsed?"parsed":"not parsed") << " }\n";
+		failmsg() << "\n";
+		return nodeResult(false);
+	}
+}
+
+// Handles different math operations like min, max
+nodeResult ARBMathFunctionNode(TIntermAggregate* node) {
+	// Generate result variable and instruction list from children
+
+	ARBVar result = ARBVar::NewTempVar();
+
+	//get the parameters
+	TIntermSequence& seq = node->getSequence();
+
+	ARBInstructionList list;
+
+	// grouping into number of parametres
+
+	switch(seq.size()) {
+		case 2: {
+			nodeResult param1 = parseNode(seq[0]);
+			nodeResult param2 = parseNode(seq[1]);
+
+			if(param1.fullyParsed && param2.fullyParsed) {
+
+				if(node->getOp() == EOpMin || node->getOp() == EOpMax) {
+
+					TIntermTyped* param2typed = seq[1]->getAsTyped();
+					if(param2typed) {
+						ARBVar var2 = param2.var;
+
+						if (param2typed->getSize() == 1) { // param2 is a float, must be copied to all vector components
+							var2 = param2.var.swizzle("x");
+						}
+
+						list.append(param1.list);
+						list.append(param2.list);
+
+						if(node->getOp() == EOpMin) {
+							list.append(ARBMinimum(result, param1.var, var2));
+						}
+						else if(node->getOp() == EOpMax) {
+							list.append(ARBMaximum(result, param1.var, var2));
+						}
+						msg() << " " << getopname(node->getOp()) << ", " << param1.var.tostr() << " " << var2.tostr() << "\n";
+						return nodeResult(result, list);
+					}
+				}
+				else if (node->getOp() == EOpPow) {
+					list.append(param1.list);
+					list.append(param2.list);
+					list.append(ARBPower(result, param1.var, param2.var));
+
+					msg() << " " << getopname(node->getOp()) << ", " << param1.var.tostr() << " " << param2.var.tostr() << "\n";
+					return nodeResult(result, list);
+				}
+				else if (node->getOp() == EOpDot) {
+					list.append(param1.list);
+					list.append(param2.list);
+
+					TIntermTyped* typed = seq[0]->getAsTyped();
+					if (!typed) {
+						failmsg() << "First parameter node to dot() not a typed value!\n";
+					}
+					if (typed->getSize() == 3) {
+						list.append(ARBDot(result, param1.var, param2.var, true));
+					}
+					else {
+						list.append(ARBDot(result, param1.var, param2.var, false));
+					}
+
+					msg() << " " << getopname(node->getOp()) << ", " << param1.var.tostr() << " " << param2.var.tostr() << "\n";
+					return nodeResult(result, list);
+				}
+				else if (node->getOp() == EOpDistance) {
+					list.append(param1.list);
+					list.append(param2.list);
+
+					TIntermTyped* typed = seq[0]->getAsTyped();
+					if (!typed) {
+						failmsg() << "First parameter node to distance() not a typed value!\n";
+						return nodeResult(false);
+					}
+					if (typed->getSize() == 3) {
+						list.append(ARBDistance(result, param1.var, param2.var, true));
+					}
+					else {
+						list.append(ARBDistance(result, param1.var, param2.var, false));
+					}
+
+					msg() << " " << getopname(node->getOp()) << ", " << param1.var.tostr() << " " << param2.var.tostr() << "\n";
+					return nodeResult(result, list);
+				}
+				failmsg() << " unhandled function call " << getopname(node->getOp()) << " on line " << node->getLine() << "\n";
+				failmsg() << "   first param: " << param1.var.tostr() << " { " << (param1.fullyParsed?"parsed":"not parsed") << " }\n";
+				failmsg() << "   second param: " << param2.var.tostr() << " { " << (param2.fullyParsed?"parsed":"not parsed") << " }\n";
+				failmsg() << "\n";
+				return nodeResult(false);
+				}
+				break;
+			}
+			case 3: {
+				nodeResult param1 = parseNode(seq[0]);
+				nodeResult param2 = parseNode(seq[1]);
+				nodeResult param3 = parseNode(seq[2]);
+
+				if(param1.fullyParsed && param2.fullyParsed && param3.fullyParsed) {
+
+					if(node->getOp() == EOpClamp) {
+
+						TIntermTyped* param2typed = seq[1]->getAsTyped();
+						TIntermTyped* param3typed = seq[2]->getAsTyped();
+
+						if(param2typed && param3typed) {
+							ARBVar var2 = param2.var;
+							ARBVar var3 = param3.var;
+
+							if (param2typed->getSize() == 1 && param3typed->getSize() == 1) { // param2  and param3 is a float, must be copied to all vector components
+								var2 = param2.var.swizzle("x");
+								var3 = param3.var.swizzle("x");
+							}
+
+							list.append(param1.list);
+							list.append(param2.list);
+							list.append(param3.list);
+
+							list.append(ARBClamp(result, param1.var, var2, var3));
+
+							msg() << " " << getopname(node->getOp()) << ", " << param1.var.tostr() << " " << var2.tostr() << " " << var3.tostr() << "\n";
+							return nodeResult(result, list);
+						}
+					}
+				} else {
+					failmsg() << " unhandled function call " << getopname(node->getOp()) << " on line " << node->getLine() << "\n";
+					failmsg() << "   first param: " << param1.var.tostr() << " { " << (param1.fullyParsed?"parsed":"not parsed") << " }\n";
+					failmsg() << "   second param: " << param2.var.tostr() << " { " << (param2.fullyParsed?"parsed":"not parsed") << " }\n";
+					failmsg() << "   third param: " << param3.var.tostr() << " { " << (param3.fullyParsed?"parsed":"not parsed") << " }\n";
+					failmsg() << "\n";
+					return nodeResult(false);
+				}
+				break;
+			}
+			default:
+                failmsg() << "Error in ARBMathNode, uknown operator " << getopname(node->getOp()) << "\n";
+				return nodeResult(false);
+		}
+		return nodeResult(false);
+}
+
+
+// Handles assignment, i.e. a = b
+nodeResult ARBAssignmentNode(TIntermBinary* node) {
+	nodeResult left = parseNode(node->getLeft());
+	nodeResult right = parseNode(node->getRight());
+
+	if (left.fullyParsed && right.fullyParsed) {
+		ARBInstructionList list;
+
+		list.append(left.list);
+		list.append(right.list);
+		list.append(ARBAssignment(left.var, right.var));
+
+		msg() << " Assignment, " << left.var.tostr() << " = " << right.var.tostr() << "\n";
+		return nodeResult(list);
+	} else {
+		failmsg() << " unhandled assignment, [" << ARBVar::GetNodeVar(node->getLeft()).tostr() << "] = [" << ARBVar::GetNodeVar(node->getRight()).tostr() << "] on line " << node->getLine() << "\n";
+		failmsg() << "   left: " << left.var.tostr() << " { " << (left.fullyParsed?"parsed":"not parsed") << " }\n";
+		failmsg() << "   right: " << right.var.tostr() << " { " << (right.fullyParsed?"parsed":"not parsed") << " }\n";
+		failmsg() << "\n";
+		return nodeResult(false);
+	}
+}
+
+// Handles matrix math operators, e.g. *
+nodeResult ARBMatrixMathNode(TIntermBinary* node) {
+	switch (node->getOp()) {
+		case EOpMatrixTimesVector: {
+			ARBVar result = ARBVar::NewTempVar();
+			ARBVar matrix = ARBVar::GetNodeVar(node->getLeft());
+			nodeResult right = parseNode(node->getRight());
+
+			ARBVar row0 = matrix.getMatrixCol(0);
+			ARBVar row1 = matrix.getMatrixCol(1);
+			ARBVar row2 = matrix.getMatrixCol(2);
+			ARBVar row3 = matrix.getMatrixCol(3);
+
+			ARBInstructionList list;
+			list.append(right.list);
+			list.append(ARBMatrixTimesVector(result, right.var, row0, row1, row2, row3));
+
+			msg() << "Matrix times vector -> " << result.tostr() << "\n";
+			return nodeResult(result, list);
+		}
+		default:
+			failmsg() << "Matrix math operator " << getopname(node->getOp()) << " not handled yet\n\n";
+			return nodeResult(false);
+	}
+}
+
+// Handles sequence of nodes by compiling each of them
+nodeResult ARBSequenceNode(TIntermAggregate* node) {
+	TIntermSequence& seq = node->getSequence();
+	if (seq.size()) {
+		ARBVar result = ARBVar::None;
+		ARBInstructionList list;
+		for (unsigned int i = 0; i < seq.size(); ++i) {
+			nodeResult res = parseNode(seq[i]);
+			list.append(res.list);
+			result = res.var;
+		}
+		return nodeResult(result, list);
+	} else {
+		return nodeResult(ARBInstructionList());
+	}
+}
+
+// Handles vector swizzling, e.g. var.yyx
+nodeResult ARBSwizzleNode(TIntermBinary* node) {
+	nodeResult res = parseNode(node->getLeft());
+	TIntermAggregate* right = node->getRight()->getAsAggregate();
+	if (res.fullyParsed && right) {
+		TIntermSequence& seq = right->getSequence();
+		string swizzle = "";
+		for (unsigned int i = 0; i < seq.size(); ++i) {
+			TIntermConstantUnion* cu = seq[i]->getAsConstantUnion();
+			if (cu && cu->getBasicType() == EbtInt && cu->getNominalSize() == 1) {
+				int i = cu->getUnionArrayPointer()[0].getIConst();
+				if (i >= 0 && i <= 4) {
+					swizzle += ARBVar::swizzleChars[i];
+				} else {
+					failmsg() << "Swizzling element " << i << " not possible\n";
+				}
+			} else {
+				failmsg() << "Swizzling " << res.var.tostr() << " failed\n";
+				return nodeResult(false);
+			}
+		}
+		ARBVar var = res.var.swizzle(swizzle);
+		return nodeResult(var, res.list);
+	}
+	return nodeResult(false);
+}
+
+// Handles vector constructors, e.g. vec4(var, 1.0)
+nodeResult ARBConstructVectorNode(TIntermAggregate* node) {
+	if (node->getBasicType() != EbtFloat) { //TODO: implementer
+		failmsg() << "Non-float vector constructors not implemented\n";
+		return nodeResult(false);
+	}
+	TIntermSequence& seq = node->getSequence();
+	ARBVar var = ARBVar::NewTempVar();
+	ARBInstructionList list;
+	int elementsFilled = 0;
+	for (unsigned int i = 0; i < seq.size(); ++i) {
+		TIntermTyped* typed = seq[i]->getAsTyped();
+		if (typed) {
+			string swizzle = "";
+			for (int j = 0; j < typed->getNominalSize(); ++j) {
+				swizzle += ARBVar::swizzleChars[elementsFilled++];
+			}
+			msg() << "Param " << i << " size: " << typed->getNominalSize() << " (" << swizzle << ")\n";
+			nodeResult res = parseNode(typed);
+			list.append(res.list);
+			list.append(ARBAssignment(var.swizzle(swizzle), res.var));
+		} else {
+			failmsg() << "Non-typed node used as input to constructor\n";
+			return nodeResult(false);
+		}
+	}
+	return nodeResult(var, list);
+}
