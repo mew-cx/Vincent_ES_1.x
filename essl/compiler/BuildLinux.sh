08bab592d6558832e0ba81d6d24d892d32f24765
houeland <houeland@01b124fe-3611-0410-befa-fcf6deab5fe0>
2006-02-16 14:08:26 +0000

    Removed warning about recent flex versions

--- essl/compiler/BuildLinux.sh
+++ essl/compiler/BuildLinux.sh
@@ -1,10 +1,5 @@
 #! /bin/bash
 
-echo Some versions of flex do not handle the glslang.l file properly, in this case the lines:
-echo '  extern int yylineno;'
-echo '  int yylineno = 1;'
-echo have to be removed from the file Gen_glslang.cpp and the last section of the glslang.l file must be appended at the end.
-
 # build the StandAlone app and all it's dependencies
 make -C StandAlone
 
