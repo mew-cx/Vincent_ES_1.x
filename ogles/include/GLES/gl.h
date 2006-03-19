c52e0b5661d716acf7d67215674c27aaafa1b06c
Hans-Martin Will <hmwill@users.noreply.github.com>
2006-03-19 06:32:27 +0000

    removed matrix palette extension

--- ogles/include/GLES/gl.h
+++ ogles/include/GLES/gl.h
@@ -108,7 +108,7 @@ typedef void (*_GLfuncptr)();
 #define GL_OES_compressed_paletted_texture 1
 /*#define GL_OES_draw_texture             1*/
 #define GL_OES_matrix_get                 1
-#define GL_OES_matrix_palette		      1
+/*#define GL_OES_matrix_palette		      1*/
 #define GL_OES_point_size_array           1
 #define GL_OES_point_sprite               1
 #define GL_OES_read_format                1
