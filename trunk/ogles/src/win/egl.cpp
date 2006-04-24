92390e69678288b295b94a3289e66f0b43e04133
magicmaps <magicmaps@01b124fe-3611-0410-befa-fcf6deab5fe0>
2006-04-24 05:53:46 +0000

    Modified egl.cpp, stdafx.h, Surface.h, Surface.cpp to be able to compile under VS Studio .Net 2005. Three function bodies were taken out in egl.cpp that weren't referenced right now.

--- trunk/ogles/src/win/egl.cpp
+++ trunk/ogles/src/win/egl.cpp
@@ -166,11 +166,14 @@ static const struct {
 	/* OES_point_size_array */
 	FunctionEntry(glPointSizePointerOES),
 
+	
 	/* OES_matrix_palette */
+	/*
 	FunctionEntry(glCurrentPaletteMatrixOES),
 	FunctionEntry(glLoadPaletteFromModelViewMatrixOES),
 	FunctionEntry(glMatrixIndexPointerOES),
 	FunctionEntry(glWeightPointerOES),
+	*/
 
 	FunctionEntry(eglSaveSurfaceHM)
 };
